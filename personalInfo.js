4fed17dd90dbc76c7bfcb8d398cae43a62e40adb
LaMarcus White <mrlamarcuswhite@gmail.com>
2026-04-14 16:04:08 -0400

    Added screenshot file showing concatenated message in VS Code's Debug Console with no runtime errors.

--- personalInfo.js
+++ personalInfo.js
@@ -9,4 +9,4 @@ let height = 72;
 let isStudent = true;
 
 //Demonstrate type casting by converting my numeric age and height to a string when concatenating.
-console.log("Hello, my name is " + name + ". I am " + age + ", " + height + "cm tall, and it is " + isStudent + " that I am a learner.")
+console.log("Hello, my name is " + name + ". I am " + age + " years old, " + height + "cm tall, and it is " + isStudent + " that I am a learner.")
