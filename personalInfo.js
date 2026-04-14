db322dd90112ea91a8fa2544b1c51b8d8ecfbc16
LaMarcus White <mrlamarcuswhite@gmail.com>
2026-04-14 15:53:33 -0400

    Created basic HTML structure. Created JS script with Personal Info Card and concatenated message. Initial commit for remote repository.

---
+++ personalInfo.js
@@ -0,0 +1,12 @@
+// Create the Personal Info Card Script 
+ 
+"use strict";
+
+//Variables with Personal Information
+let name = "LaMarcus";
+let age = 33;
+let height = 72;
+let isStudent = true;
+
+//Demonstrate type casting by converting my numeric age and height to a string when concatenating.
+console.log("Hello, my name is " + name + ". I am " + age + ", " + height + "cm tall, and it is " + isStudent + " that I am a learner.")
